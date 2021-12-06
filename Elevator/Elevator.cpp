9bafb2a7e0b941d38c430c560b15768130b5dd1b
Andrew Kim <66102642+ahkim3@users.noreply.github.com>
2021-12-06 03:54:29 -0600

    Update

--- Elevator/Elevator.cpp
+++ Elevator/Elevator.cpp
@@ -1,138 +1,111 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+	Name: Andrew Kim
+	Date: 12/5/2021
+	Title: Programming Assignment #3
+
+	References:
+		1) https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+*/
+
 #include <iostream>
-
-// Define Global Variables and Prototypes
-#define MAX 100
-
-#define initial 1 // white
-#define waiting 2 // grey
-#define visited 3 // black
-
-int n; // no. of vertices
-int adj[MAX][MAX];
-int state[MAX];
-void create_graph();
-void BF_Traversal();
-void BFS(int v);
-
-int queue[MAX], front = -1, rear = -1;
-void insert_queue(int vertex);
-int pop_queue();
-int isEmpty_queue();
+#include <queue>
 
 using namespace std;
 
 
+// This class represents a directed graph using
+// adjacency list representation
+class Graph
+{
+	int vertices; // No. of vertices
+
+	// Pointer to an array containing adjacency
+	// lists
+	vector<int>* adj;
+public:
+	Graph(int vertices); // Constructor
+
+	// function to add an edge to graph
+	void addEdge(int v, int w);
+
+	// prints BFS traversal from a given source s
+	void BFS(int s);
+};
+
+Graph::Graph(int vertices)
+{
+	this->vertices = vertices;
+	adj = new vector<int>[vertices];
+}
+
+
+// Add the weight of a new edge to a vertex's adjacency list
+void Graph::addEdge(int vertex, int weight)
+{
+	adj[vertex].push_back(weight);
+}
+
+void Graph::BFS(int startVertex)
+{
+	// Mark all the vertices as not visited
+	bool* visited = new bool[vertices];
+	for (int i = 0; i < vertices; i++)
+		visited[i] = false;
+
+	// Create a queue for BFS
+	queue<int> queue;
+
+	// Mark the current node as visited and enqueue it
+	visited[startVertex] = true;
+	queue.push(startVertex);
+
+	// 'i' will be used to get all adjacent
+	// vertices of a vertex
+	vector<int>::iterator i;
+
+	while (!queue.empty())
+	{
+		// Dequeue a vertex from queue and print it
+		startVertex = queue.front();
+		cout << startVertex << " ";
+		queue.pop();
+
+		// Get all adjacent vertices of the dequeued
+		// vertex s. If a adjacent has not been visited,
+		// then mark it visited and enqueue it
+		for (i = adj[startVertex].begin(); i != adj[startVertex].end(); ++i)
+		{
+			if (!visited[*i])
+			{
+				visited[*i] = true;
+				queue.push(*i);
+			}
+		}
+	}
+}
+
+
 int main()
 {
-    create_graph();
-    BF_Traversal();
-    return 0;
-}
+	int floors, start, goal, up, down;
 
-// BF_Traversal Function
-void BF_Traversal()
-{
-    int v;
+	// Obtain user input
+	cout << "Please enter desired floors, start, goal, up, and down inputs: ";
+	cin >> floors >> start >> goal >> up >> down;
+	cout << endl;
 
-    for (v = 0; v < n; v++)
-        state[v] = initial;
+	// Create a graph given in the above diagram
+	Graph g(4);
+	g.addEdge(0, 1);
+	g.addEdge(0, 2);
+	g.addEdge(1, 2);
+	g.addEdge(2, 0);
+	g.addEdge(2, 3);
+	g.addEdge(3, 3);
 
-    printf("Enter Start Vertex for BFS: \n");
-    cin >> v;
-    BFS(v);
-}
+	cout << "Following is Breadth First Traversal "
+		<< "(starting from vertex 2) \n";
+	g.BFS(2);
 
-// BFS Function
-void BFS(int v)
-{
-    int i;
-
-    insert_queue(v);
-    state[v] = waiting;
-
-    while (!isEmpty_queue())
-    {
-        v = pop_queue();
-        printf("%d ", v);
-        state[v] = visited;
-
-        for (i = 0; i < n; i++)
-        {
-            if (adj[v][i] == 1 && state[i] == initial)
-            {
-                insert_queue(i);
-                state[i] = waiting;
-            }
-        }
-    }
-    printf("\n");
-}
-
-
-// insert_queue Function
-void insert_queue(int vertex)
-{
-    if (rear == MAX - 1)
-        printf("Queue Overflow\n");
-    else
-    {
-        if (front == -1)
-            front = 0;
-        rear = rear + 1;
-        queue[rear] = vertex;
-    }
-}
-
-// isEmpty_queue Function
-int isEmpty_queue()
-{
-    if (front == -1 || front > rear)
-        return 1;
-    else
-        return 0;
-}
-
-// Pop_queue Function
-int pop_queue()
-{
-    int pop_item;
-    if (front == -1 || front > rear)
-    {
-        printf("Queue Underflow\n");
-        exit(1);
-    }
-
-    pop_item = queue[front];
-    front = front + 1;
-    return pop_item;
-}
-
-// create_graph Function
-void create_graph()
-{
-    int count, max_edge, origin, destin;
-
-    printf("Enter Number of Vertices : ");
-    cin >> n;
-    max_edge = n * (n - 1);
-
-    for (count = 1; count <= max_edge; count++)
-    {
-        cout << "Enter Edge " << count << " (-1 -1 to quit) :";
-        cin >> origin >> destin;
-
-        if ((origin == -1) && (destin == -1))
-            break;
-        if (origin >= n || destin >= n || origin < 0 || destin < 0)
-        {
-            printf("Invalid Edge!\n");
-            count--;
-        }
-        else
-        {
-            adj[origin][destin] = 1;
-        }
-    }
+	return 0;
 }
